2c2c6c73f5f9c345b82f73d005e71285699f406a
Aburabura <pwhitby616@gmail.com>
2026-05-27 21:05:52 -0700

    complete platform.cpp and platform.hpp

--- platform.hpp
+++ platform.hpp
@@ -7,14 +7,14 @@ class Platform
     friend class imgui;
 
     public:
-        platform(char const* title, int windowwidth, int windowHeight, int texturewidth, int textureHeight);
+        Platform(char const* title, int windowwidth, int windowHeight, int texturewidth, int textureHeight);
         ~Platform();
         void update(void const* buffer, int pitch);
         bool ProcessInput(uint8_t* keys);
 
 
     private:
-        SDL_window* window{};
+        SDL_Window* window{};
         SDL_GLContext gl_context{};
         GLuint framebuffer_texture;
         SDL_Renderer* renderer{};
